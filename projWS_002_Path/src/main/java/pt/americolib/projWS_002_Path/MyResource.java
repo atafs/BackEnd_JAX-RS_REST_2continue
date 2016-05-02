652bbd03517f0643bf17569b96bdc99d6bf82f01
atafs <atafs@iscte-iul.pt>
2016-05-02 19:50:29 +0100

    comiit

--- projWS_002_Path/src/main/java/pt/americolib/projWS_002_Path/MyResource.java
+++ projWS_002_Path/src/main/java/pt/americolib/projWS_002_Path/MyResource.java
@@ -2,24 +2,63 @@ package pt.americolib.projWS_002_Path;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
-@Path("myresource")
+@Path("ws")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it!";
     }
+    
+    @GET
+	@Path("/vip")
+	public Response getUserVIP() {
+
+		return Response.status(200).entity("getUserVIP is called").build();
+
+	}
+    
+    @GET
+	@Path("{name}")
+	public Response getUserByName(@PathParam("name") String name) {
+
+		return Response.status(200)
+			.entity("getUserByName is called, name : " + name).build();
+
+	}
+    
+    @GET
+	@Path("{id : \\d+}") //support digit only
+	public Response getUserById(@PathParam("id") String id) {
+
+	   return Response.status(200).entity("getUserById is called, id : " + id).build();
+
+	}
+
+	@GET
+	@Path("/username/{username : [a-zA-Z][a-zA-Z_0-9]}")
+	public Response getUserByUserName(@PathParam("username") String username) {
+
+	   return Response.status(200)
+		.entity("getUserByUserName is called, username : " + username).build();
+
+	}
+	
+	@GET
+	@Path("/books/{isbn : \\d+}")
+	public Response getUserBookByISBN(@PathParam("isbn") String isbn) {
+
+	   return Response.status(200)
+		.entity("getUserBookByISBN is called, isbn : " + isbn).build();
+
+	}
+    
+    
+    
 }
--- projWS_003_QueryParam/src/main/java/pt/americolib/projWS_003_QueryParam/MyResource.java
+++ projWS_003_QueryParam/src/main/java/pt/americolib/projWS_003_QueryParam/MyResource.java
@@ -1,25 +1,77 @@
 package pt.americolib.projWS_003_QueryParam;
 
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("ws")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
-    }
+	@GET
+	@Path("{year}/{month}/{day}")
+	public Response getUserHistory(
+			@PathParam("year") int year,
+			@PathParam("month") int month, 
+			@PathParam("day") int day) {
+
+	   String date = year + "/" + month + "/" + day;
+
+	   return Response
+			   .status(200)
+			   .entity("getUserHistory is called, year/month/day : " + date).build();
+
+	}
+	
+	@GET
+	@Path("/query1")
+	public Response getUsers(
+		@QueryParam("from") int from,
+		@QueryParam("to") int to,
+		@QueryParam("orderBy") List<String> orderBy) {
+
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
+	}
+	
+	@GET
+	@Path("/query2")
+	public Response getUsers(@Context UriInfo info) {
+
+		String from = info.getQueryParameters().getFirst("from");
+		String to = info.getQueryParameters().getFirst("to");
+		List<String> orderBy = info.getQueryParameters().get("orderBy");
+		
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
+	}
+	
+	@GET
+	@Path("/query3")
+	public Response getUsers3(
+		@DefaultValue("1000") @QueryParam("from") int from,
+		@DefaultValue("999")@QueryParam("to") int to,
+		@DefaultValue("name") @QueryParam("orderBy") List<String> orderBy) {
+
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+
+	}
 }
